00b66d5f468e4aae8bcc10c27b62c94fa6e81476
= <=>
2022-06-30 16:21:34 +0100

    enviar as requests no endpoint da API local

--- src/services/API.js
+++ src/services/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sistemaloginfetapi.herokuapp.com/api",
+  baseURL: "http://localhost:8000/api",
 });
 
 api.interceptors.response.use(function (response) {
